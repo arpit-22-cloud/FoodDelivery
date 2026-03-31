ec2848da11c2e1ab340cf505bd4826f748a2adc9
ArpitNagar <nagararpit22@gmail.com>
2026-03-31 18:02:07 +0530

    add logo

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,6 +3,6 @@ import mongoose from "mongoose";
 // Function to establish connection with MongoDB Atlas
 export const connectDB = async () => {
     
-    await mongoose.connect('mongodb+srv://ZippyBite:ZippyBite123@cluster0.yfgwlll.mongodb.net/ZippyBite')
+    await mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log("DB Connected")); // Log success message once connected
 }
